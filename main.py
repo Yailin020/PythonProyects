b38842ea08d9a116bfca5ee95129c8f80c505d27
Yailin Caro <ye.pacheco21@info.uas.edu.mx>
2026-02-15 18:20:50 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import datetime, bday_messages
+today = datetime.date.today()
+next_birthday = datetime.date(2027, 1, 20)
+days_away = next_birthday - today
+
+if today == next_birthday:
+    print(bday_messages.random_message)
+else: print(f'My next birthday is {days_away.days} days away!')
